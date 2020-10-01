860a6f15f8d69494e4433c3ebd4cc1658350e640
Aryav <tiwarikaustav@gmail.com>
2020-10-01 07:56:58 +0530

    The Die has been rolled

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace SnakeLadderProblem
         static void Main(string[] args)
         {
             const int START_POSITION = 0;
+            int dieNum = RandomNumber();
+            Console.WriteLine("Die Number is: {0}", dieNum);
+        }
+
+        //returns 1 to 6, rolling of a die
+        static int RandomNumber()
+        {
+            Random randObj = new Random();
+            return randObj.Next(1, 7);
         }
     }
 }
